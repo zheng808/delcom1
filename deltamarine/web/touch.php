ad3b4157cd76b44d4176faf9a63ee22fb9678c03
Jacques Putter <jacques.putter@gmail.com>
2019-02-07 17:13:02 -0800

    Set Touch app to Production

--- deltamarine/web/touch.php
+++ deltamarine/web/touch.php
@@ -4,7 +4,7 @@ require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
 $app = 'touch';
 $env = 'prod';
-$debug = false;
+$debug = true;
 
 //load the appropriate app (doesn't check for existence, so will throw error if invalid one specified)
 $configuration = ProjectConfiguration::getApplicationConfiguration($app, $env, $debug);
